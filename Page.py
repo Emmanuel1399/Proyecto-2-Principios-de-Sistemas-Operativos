c0589e1e8340f2e87704ac846b4295a4d453f258
Emmanuel <emma_1399@icloud.com>
2024-05-05 18:03:03 -0600

    Modifications

--- Page.py
+++ Page.py
@@ -1,6 +1,5 @@
 class Page:
     size = 4  # capacidad de paginas en KB
-    def __init__(self, identificador):
-        self.id = identificador
-        self.virtual_memory = False
+    def __init__(self, id):
+        self.id = id
 
