a2a4caafba4aa3c292592b0fb1922b10601fd304
Emmanuel <emma_1399@icloud.com>
2024-05-05 20:35:31 -0600

    Modifications

--- Pointer.py
+++ Pointer.py
@@ -1,8 +1,13 @@
 class Pointer:
-    def __init__(self,pid,size):
+    def __init__(self, pid, size):
         self.page_list = []
         self.pid = pid
         self.size = size
         self.kill = False
+
     def delete(self):
+        waste = 0
+        for page in self.page_list:
+            waste += page.waste
         self.page_list = []
+        return waste
